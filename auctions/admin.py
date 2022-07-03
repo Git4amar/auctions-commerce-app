98bdb547eb46a4730689304fe383da6bb2af905a
Git4amar <apsbir@icloud.com>
2022-07-02 23:32:12 -0700

    Meet all specs V1

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
+
+class ListingsAdmin(admin.ModelAdmin):
+    list_display = ("title", "startingBid", "listingMaxBid")
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("listing", "bid")
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("listing", "comment")
 
 # Register your models here.
+admin.site.register(auctionListings, ListingsAdmin)
+admin.site.register(bids, BidAdmin)
+admin.site.register(comments, CommentAdmin)
