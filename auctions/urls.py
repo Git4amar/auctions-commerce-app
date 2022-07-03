c86f231b68926c1c4162bf40b292542acf10c669
Git4amar <apsbir@icloud.com>
2022-07-02 23:31:34 -0700

    Meet all specs V1

--- auctions/urls.py
+++ auctions/urls.py
@@ -4,7 +4,12 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("closed", views.closed, name="closed"),
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register")
+    path("register", views.register, name="register"),
+    path("create", views.createListing, name="createListing"),
+    path("listing/<str:listingID>", views.listing, name="listing"),
+    path("watchlist", views.watchlist, name="watchlist"),
+    path("categories/<str:category>", views.categories, name="categories")
 ]
